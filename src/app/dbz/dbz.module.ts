dc17f8a96b859effc24223757c65ee3f01770ff2
Eduardo Passano Chávez <eapch21@gmail.com>
2022-11-13 23:23:35 -0500

    62. Crear componentes hijos

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PersonajesComponent],
   imports: [
     CommonModule,
     FormsModule
